c59120bb20c136a8ee46a0bb0a4c26f1b027a08d
vspeed21 <vtorresquintanilla0206@gmail.com>
2022-11-24 15:29:50 -0600

    controllers: addNewAdmin created

--- src/controllers/adminControllers.ts
+++ src/controllers/adminControllers.ts
@@ -3,5 +3,24 @@ import { RequestHandler } from 'express';
 import Admin from "../models/Admin";
 
 export const addNewAdmin: RequestHandler = async (req, res) => {
-	res.send('hola express con eslint');
+  const { email } = req.body;
+
+  const adminExists = await Admin.findOne({email});
+
+  if(adminExists) {
+    const error = new Error('Cuenta ya registrada');
+    return res.status(400).json({msg: error.message});
+  }
+
+	try {
+    const admin = new Admin(req.body);
+    await admin.save();
+
+    //Enviar email de registro
+
+    res.json({msg: 'Se ha enviado un email para confirmar tu cuenta'});
+    
+  } catch (error) {
+    console.log(error);
+  }
 }
