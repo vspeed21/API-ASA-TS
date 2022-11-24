dd4e25ffaf1882b0e7ab6194219dd122f2097650
vspeed21 <vtorresquintanilla0206@gmail.com>
2022-11-24 14:26:22 -0600

    change string mongo uri to variables env

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,8 +1,9 @@
 import { connect } from "mongoose";
+import config from '../config';
 
 async function connectDB() {
   try {
-    const db = await connect('mongodb+srv://tikoDev21:sisi20012019VRMONGO@cluster.6kdr2.mongodb.net/?retryWrites=true&w=majority');
+    const db = await connect(config.MONGO_URI);
 
     const url = `${db.connection.host}:${db.connection.port}`;
     console.log(`Mongo connected to ${url}`);
