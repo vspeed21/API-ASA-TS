ba2433bff80544435ed4108aef340184f9255464
vspeed21 <vtorresquintanilla0206@gmail.com>
2022-11-24 14:46:13 -0600

    routes & controllers for Admin created

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
 import express from 'express';
-import AdminRoutes from './routes/AdminRoutes';
+import adminRoutes from './routes/AdminRoutes';
 
 const app = express();
 
-app.use('/api/admin', AdminRoutes);
+app.use('/api/admin', adminRoutes);
 
 
 export default app;
