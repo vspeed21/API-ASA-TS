f9858b08487917d6a8e7c9d50b7fad3c0e3ae8af
vspeed21 <vtorresquintanilla0206@gmail.com>
2022-11-25 21:59:40 -0600

    routes & controllers for confirmAcc done

--- src/controllers/adminControllers.ts
+++ src/controllers/adminControllers.ts
@@ -1,26 +1,54 @@
 import { RequestHandler } from 'express';
+import { IAdmin } from '../interface';
+import mongoose from 'mongoose';
 
 import Admin from "../models/Admin";
 
 export const addNewAdmin: RequestHandler = async (req, res) => {
-  const { email } = req.body;
+	const { email } = req.body;
 
-  const adminExists = await Admin.findOne({email});
+	const adminExists:IAdmin | null  = await Admin.findOne({ email });
 
-  if(adminExists) {
-    const error = new Error('Cuenta ya registrada');
-    return res.status(400).json({msg: error.message});
-  }
+	if (adminExists) {
+		const error = new Error('Cuenta ya registrada');
+		return res.status(400).json({ msg: error.message });
+	}
 
 	try {
-    const admin = new Admin(req.body);
-    await admin.save();
+		const admin = new Admin(req.body);
+		await admin.save();
 
-    //Enviar email de registro
+		//Enviar email de registro
 
-    res.json({msg: 'Se ha enviado un email para confirmar tu cuenta'});
-    
-  } catch (error) {
-    console.log(error);
-  }
+		res.json({ msg: 'Se ha enviado un email para confirmar tu cuenta' });
+
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+export const confirmAccount: RequestHandler = async (req, res) => {
+	const isValid = mongoose.isObjectIdOrHexString(req.params.id);
+
+	if(!isValid) {
+		const error = new Error('Cuenta no encontrada');
+		return res.status(404).json({msg: error.message});
+	}
+
+	const admin:IAdmin | any  = await Admin.findById({_id: req.params.id});
+
+	try {
+		if(admin != null) {
+			admin.token = null;
+			admin.confirmed = true;
+			await admin.save();
+		}
+
+		res.status(200).json({msg: 'Cuenta confirmada correctamente'});
+
+		
+	} catch (error) {
+		console.log(error);
+	}
+
 }
--- src/routes/adminRoutes.ts
+++ src/routes/adminRoutes.ts
@@ -3,6 +3,9 @@ import * as admin from '../controllers/adminControllers';
 
 const router = Router();
 
+//Public Requests
 router.post('/', admin.addNewAdmin);
+router.get('/confirm/:id', admin.confirmAccount);
+
 
 export default router;
