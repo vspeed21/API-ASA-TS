1b72a1f39c24bcf4972aff6e821899106b6687c1
vspeed21 <vtorresquintanilla0206@gmail.com>
2022-12-27 22:08:54 -0600

    Profile interface created

--- src/interface/index.ts
+++ src/interface/index.ts
@@ -1,7 +1,17 @@
+import { Schema } from 'mongoose';
+
 export interface IAdmin {
   name: string,
   email: string,
   password: string,
   token: string | null,
   confirmed: boolean,
+}
+
+export interface IProfile {
+  name: string,
+  screen: string,
+  pin: number
+  deadline: string,
+  admin: Schema.Types.ObjectId,
 }
