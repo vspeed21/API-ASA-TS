b3e184e85fc2d98fd96268a70b6cdb4d26277739
vspeed21 <vtorresquintanilla0206@gmail.com>
2022-12-30 15:02:33 -0600

    configuring cors

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,26 @@
 import express from 'express';
+import cors, { CorsOptions } from 'cors';
+
 import adminRoutes from './routes/adminRoutes';
 import profileRoutes from './routes/profileRoutes';
 
 const app = express();
 
+const whitelist = [process.env.FRONTEND_URL];
+
+const corsOptions:CorsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}
+
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(cors(corsOptions));
 
 app.use('/api/admin', adminRoutes);
 app.use('/api/profile', profileRoutes);
