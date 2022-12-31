b690a083ed92be8cefbbe2d201d52ad9a78f4755
vspeed21 <vtorresquintanilla0206@gmail.com>
2022-12-30 21:59:43 -0600

    add new env for auth

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,9 @@ dotenv.config();
 
 const config = {
   MONGO_URI: process.env.MONGO_URI ?? '',
-  JWT_SECRET: process.env.JWT_SECRET ?? ''
+  JWT_SECRET: process.env.JWT_SECRET ?? '',
+  USER: process.env.USER ?? '',
+  PASS: process.env.PASS ?? '',
 }
 
 export default config;
