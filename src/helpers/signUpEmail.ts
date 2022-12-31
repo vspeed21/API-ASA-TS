00cb43db29474e62e119fa5953169ad2e5bc7420
vspeed21 <vtorresquintanilla0206@gmail.com>
2022-12-30 22:36:55 -0600

    helpers: signUpEmail function created

---
+++ src/helpers/signUpEmail.ts
@@ -0,0 +1,36 @@
+import nodemailer from 'nodemailer';
+import config from '../config';
+
+interface Data {
+  name: string,
+  email: string,
+  token: string | null,
+  host: string,
+}
+
+async function sendEmailSignUp({name, email, token, host}:Data):Promise<void> {
+  const transport = nodemailer.createTransport({
+    host: `smtp.${host}.com`,
+    port: 465,
+    secure: true,
+    auth: {
+      user: config.USER,
+      pass: config.PASS,
+    }
+  });
+
+  const info = await transport.sendMail({
+    from: 'Admin Streaming account (ASA)',
+    to: email,
+    subject: 'Confirm your account in Admin Streaming Account (ASA)',
+    html: `<p>Dear ${name}, confirm your account to start managing your profiles.</p>
+    <p>Confirm in the following link: <a href="${process.env.FRONTEND_URL}/confirm/${token}">Confirm</a></p>
+
+    <p>If you didn't create this account, ignore this message.</p>
+    `,
+  });
+
+  console.log("Message sent: %s", info.messageId);
+}
+
+export default sendEmailSignUp
