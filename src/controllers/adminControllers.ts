4615c90c1eb90012e3b679719832dc7717a54c42
vspeed21 <vtorresquintanilla0206@gmail.com>
2022-12-30 21:37:19 -0600

    adminCtrl: change response language spanish to english

--- src/controllers/adminControllers.ts
+++ src/controllers/adminControllers.ts
@@ -12,8 +12,8 @@ export const addNewAdmin: RequestHandler = async (req, res) => {
 	const adminExists:IAdmin | null  = await Admin.findOne({ email });
 
 	if (adminExists) {
-		const error = new Error('Cuenta ya registrada');
-		return res.status(201).json({ msg: error.message });
+		const error = new Error('Account already registered. Log in');
+		return res.status(404).json({ msg: error.message });
 	}
 
 	try {
@@ -22,7 +22,7 @@ export const addNewAdmin: RequestHandler = async (req, res) => {
 
 		//Enviar email de registro
 
-		res.json({ msg: 'Se ha enviado un email para confirmar tu cuenta' });
+		res.status(201).json({ msg: 'Check your email to confirm your account' });
 
 	} catch (error) {
 		console.log(error);
@@ -35,7 +35,7 @@ export const confirmAccount: RequestHandler = async (req, res) => {
 	const admin = await Admin.findOne({token});
 
 	if(!admin) {
-		const error = new Error('Cuenta no encontrada');
+		const error = new Error('Account not found');
 		return res.status(404).json({msg: error.message});
 	}
 
@@ -46,7 +46,7 @@ export const confirmAccount: RequestHandler = async (req, res) => {
 			await admin.save();
 		}
 
-		res.status(200).json({msg: 'Cuenta confirmada correctamente'});
+		res.status(200).json({msg: 'Account confirmed successfully'});
 
 		
 	} catch (error) {
@@ -61,12 +61,12 @@ export const forgotPasswordSendEmail = async (req:Request, res:Response) => {
 	const admin:IAdmin | any = await Admin.findOne({email});
 
 	if(!admin) {
-		const error = new Error('Cuenta no registrada aún');
+		const error = new Error('Account not registered yet');
 		return res.status(404).json({ msg: error.message });
 	}
 
 	if(admin.token !== null) {
-		const error = new Error('Ya se ha enviado un correo');
+		const error = new Error('Email has already been sent');
 		return res.status(429).json({ msg: error.message });
 	}
 
@@ -76,7 +76,7 @@ export const forgotPasswordSendEmail = async (req:Request, res:Response) => {
 			await admin.save();
 		}
 
-		res.status(200).json({msg: 'Se ha enviado un correo con las instrucciones'});
+		res.status(200).json({msg: 'Email has been sent'});
 
 		
 	} catch (error) {
@@ -94,7 +94,7 @@ export const checkToken = async (req:Request, res:Response) => {
 		return res.status(404).json({msg: error.message});
 	}
 
-	res.json({msg: 'Escribe la nueva contraseña'});
+	res.json({msg: 'Type your new password'});
 }
 
 export const savePassword = async (req:Request, res:Response) => {
@@ -103,12 +103,12 @@ export const savePassword = async (req:Request, res:Response) => {
 	const admin:IAdmin | any = await Admin.findOne({token: req.params.token});
 
 	if(admin === null) {
-		const error = new Error('Cuenta no encontrada');
+		const error = new Error('Account not found');
 		return res.status(404).json({msg: error.message});
 	}
 
 	if(await admin.checkPassword(password)) {
-		const error = new Error('La nueva contraseña no puede ser igual que la anterior');
+		const error = new Error("The new password can't be the same as the old one");
 		return res.status(400).json({msg: error.message});
 	}
 
@@ -117,7 +117,7 @@ export const savePassword = async (req:Request, res:Response) => {
 		admin.password = password;
 		await admin.save();
 
-		res.status(200).json({msg: 'Contraseña modificada correctamente'});
+		res.status(200).json({msg: 'Password modified successfully'});
 		
 	} catch (error) {
 		console.log(error);
@@ -131,12 +131,12 @@ export const login = async (req:Request, res:Response) => {
 
 	
 	if(admin === null) {
-		const error = new Error('Cuenta no registrada aún');
+		const error = new Error('Account not registered yet');
 		return res.status(404).json({msg: error.message});
 	}
 
 	if(!admin.confirmed) {
-		const error = new Error('Tu cuenta no ha sido confirmada aún');
+		const error = new Error('Account has not been confirmed yet');
 		return res.status(404).json({msg: error.message});
 	}
 
